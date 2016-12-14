c339c41fa66de518251880e53b3175e9b60445f2
Cristián Rojas <injcristianrojas@gmail.com>
2016-12-14 16:40:58 -0300

    Ordering done

---
+++ app/header.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+$is_session_started = isset($_SESSION['username']);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<!-- this template was designed by http://www.tristarwebdesign.co.uk - please visit for more templates & information - thank you. -->
+<head>
+<title>Fans de las aves chilenas (SWSEC Intro)</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+
+<div id="border">
+	<div id="container">
+
+		<div id="menu">
+			<ul>
+        <?php if ($is_session_started) { ?>
+				<li>Usuario: <?php echo $_SESSION["username"] ?></li><li>|</li>
+				<?php } ?>
+        <li><a title="Home" id="home" href="index.php">Home</a></li><li>|</li>
+				<?php if (!$is_session_started) { ?>
+				<li><a title="Ingresar" id="login" href="login.php">Ingresar</a></li><li>|</li>
+				<?php } ?>
+				<li><a title="Saludos" id="hello" href="saludos.php">Saludos</a></li>
+				<?php if ($is_session_started) { ?>
+				<li>|</li>
+				<li><a title="Muro" id="wall" href="Wall">Muro</a></li><li>|</li>
+				<li><a title="Imagenes" id="images" href="pictures.php">Imagenes</a></li><li>|</li>
+				<li><a title="Usuarios" id="users" href="lista_usuarios.php?type=1">Usuarios</a></li><li>|</li>
+				<li><a title="Cuenta" id="account" href="cuenta.php">Cuenta</a></li><li>|</li>
+				<li><a title="Salir" id="exit" href="index.php?kill_session=1">Salir</a></li>
+				<?php } ?>
+			</ul>
+		</div>
+
+		<div id="header">
+			<p><a title="website name" href="index.php">Fans de las aves chilenas</a></p>
+		</div>
+
+		<div id="content">
--- app/index.php
+++ app/index.php
@@ -1,81 +1,59 @@
 <?php
-
-  if (!file_exists('db/birds_fans.sqlite'))
-    include_once('db/db_startup.php');
-
-  session_start();
-  $is_session_started = isset($_SESSION['username']);
-  $expire_session = isset($_GET['kill_session']);
-  $is_post = $_SERVER['REQUEST_METHOD'] === 'POST';
-
-  if ($expire_session) {
-    session_unset();
-    session_destroy();
-    header('Location: /index.php');
-  }
-
-  if ($is_post) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if ($dbh = new SQLite3('db/birds_fans.sqlite')) {
-      $result = $dbh->query("select * from usuarios where username = '$username'
-        and password = '$password'");
-      $nrows = 0;
-      while ($result->fetchArray()) $nrows++;
-      if ($nrows > 0) {
-        $_SESSION['username'] = $username;
-        header('Location: /index.php');
-        die();
-      }
-    } else {
-      die($error);
-    }
-  }
+  include_once('header.php');
 ?>
 
-<!doctype html>
-<html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <title>Fans de las Aves Chilenas</title>
-    <meta name="description" content="Introducción a la Seguridad de Software">
-    <meta name="author" content="Cristián Rojas">
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-  </head>
+<?php
+if (!file_exists('db/birds_fans.sqlite'))
+  include_once('db/db_startup.php');
 
-  <body>
-  <div id="border">
-  	<div id="container">
-  		<div id="menu">
-  			<ul>
-  				<li><a title="Home" id="home" href="/index.php">Home</a></li>
-  			</ul>
-  		</div>
+$expire_session = isset($_GET['kill_session']);
+$is_post = $_SERVER['REQUEST_METHOD'] === 'POST';
 
-  		<div id="header">
-  			<p><a title="website name" href="/index.php">Fans de las aves chilenas</a></p>
-  		</div>
+if ($expire_session) {
+  session_unset();
+  session_destroy();
+  header('Location: /index.php');
+}
 
-  		<div id="content">
-      <?php
-      if ($is_session_started) {
-      ?>
-        <p>Bienvenido, <?php echo $_SESSION["username"] ?>.</br>
-        <a href="index.php?kill_session=1">Cerrar sesión</a></p>
-      <?php
-      } else {
-      ?>
-        <form action="/index.php" method="post" name="mainForm" id="mainForm">
-        <p>
-        Nombre: <input type="text" name="username" id="username" /><br />
-        Password: <input type="password" name="password" id="password"  /><br />
-        <input type="submit" name="submitButton" id="submitButton" value="Ingresar" />
-        </p>
-        </form>
-      <?php
-      }
-      ?>
-    </div>
-  </body>
-</html>
+if ($is_post) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if ($dbh = new SQLite3('db/birds_fans.sqlite')) {
+    $result = $dbh->query("select * from usuarios where username = '$username'
+      and password = '$password'");
+    $nrows = 0;
+    while ($result->fetchArray()) $nrows++;
+    if ($nrows > 0) {
+      $_SESSION['username'] = $username;
+      header('Location: /index.php');
+      die();
+    }
+  } else {
+    die($error);
+  }
+}
+?>
+
+<?php
+if ($is_session_started) {
+?>
+      <p>Bienvenido, <?php echo $_SESSION["username"] ?>.</br>
+      <a href="index.php?kill_session=1">Cerrar sesión</a></p>
+<?php
+} else {
+?>
+      <form action="/index.php" method="post" name="mainForm" id="mainForm">
+      <p>
+      Nombre: <input type="text" name="username" id="username" /><br />
+      Password: <input type="password" name="password" id="password"  /><br />
+      <input type="submit" name="submitButton" id="submitButton" value="Ingresar" />
+      </p>
+      </form>
+<?php
+}
+?>
+
+<?php
+include_once('footer.inc');
+?>
